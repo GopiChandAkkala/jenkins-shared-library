ba1155a67f92a43f3e0457b0460075fabd4b6570
GopiChandAkkala <akkalagopi@gmail.com>
2023-12-10 11:37:54 +0530

    typo

--- vars/dockerHubImagePush.groovy
+++ vars/dockerHubImagePush.groovy
@@ -2,6 +2,6 @@ def call(String hubUser, String project, String imageTag){
     withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]){
         sh "docker login -u '$USER' -p '$PASS'"
     }
-    sh 'docker push ${hubUser}/${project}:${imageTag}'
-    sh 'docker push ${hubUser}/${project}:latest '
+    sh "docker push ${hubUser}/${project}:${imageTag}"
+    sh "docker push ${hubUser}/${project}:latest"
 }
